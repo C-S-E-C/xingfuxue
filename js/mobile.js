1f62578ab593813d36b91cb4163f15fd9d830c8d
ianchzhu <117727274+ianchzhu@users.noreply.github.com>
2025-07-19 10:43:03 +0000

    sdfjmhjshk.gd

--- js/mobile.js
+++ js/mobile.js
@@ -2,7 +2,7 @@ const windowInnerWidth = window.innerWidth;
 const windowInnerHeight = window.innerHeight;
 console.log("Window Width: " + windowInnerWidth);
 console.log("Window Height: " + windowInnerHeight);
-const isMobile = windowInnerHeight > windowInnerHeight;
+const isMobile = windowInnerHeight > windowInnerWidth;
 
 if (isMobile) {
     const path = window.location.pathname;
