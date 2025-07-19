5bcb9360e0ff78ffc43cc4b5a9fe45927a6e4e45
ianchzhu <117727274+ianchzhu@users.noreply.github.com>
2025-07-19 11:07:25 +0000

    mobile interval

--- mobile/js/mobile.js
+++ mobile/js/mobile.js
@@ -1,12 +1,15 @@
-const windowInnerWidth = window.innerWidth;
-const windowInnerHeight = window.innerHeight;
+function test(){
+var windowInnerWidth = window.innerWidth;
+var windowInnerHeight = window.innerHeight;
 console.log("Window Width: " + windowInnerWidth);
 console.log("Window Height: " + windowInnerHeight);
-const isMobile = windowInnerHeight > windowInnerWidth;
-// const isMobile = false;
+var isMobile = windowInnerHeight > windowInnerWidth;
+// var isMobile = false;
 if (!isMobile) {
-    const path = window.location.pathname;
-    const newPath = path.replace("/mobile","");
+    var path = window.location.pathname;
+    var newPath = path.replace("/mobile","");
     console.log("Mobile device detected, redirecting to: " + newPath);
     window.location.href = newPath;
 }
+}
+setInterval('test();', 1000);
