75c26c9351d013590814c15837ae77bd67c364ce
ianchzhu <vfd9654@yeah.net>
2025-08-04 14:04:39 +0300

    Update barba.js

--- barba/js/barba.js
+++ barba/js/barba.js
@@ -4,13 +4,15 @@ barba.init({
     leave(data) {
       // create your stunning leave animation here
       return gsap.to(data.current.container, {
-        opacity: 0
+        opacity: 0,
+        duration: 0.3
       });
     },
     enter(data) {
       // create your amazing enter animation here
       return gsap.to(data.next.container, {
-        opacity: 0
+        opacity: 0,
+        duration: 0.3
       });
     }
   }]
